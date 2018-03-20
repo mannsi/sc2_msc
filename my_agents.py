b335098f9b010b76988c8d67d2b77860e97a9859
Mannsi <mannsi1983@gmail.com>
2018-03-20 15:23:20 +0000

    Change deterministic logic since marine fire cooldown is random

--- my_agents.py
+++ my_agents.py
@@ -42,8 +42,12 @@ class MyBaseAgent(base_agent.BaseAgent):
         super().__init__()
         self.obs = None
         self._steps_without_rewards = 0
-        self.step_mul = int(helper.get_command_param_val('--step_mul', remove_from_params=False, default_val=8))
+        self._step_mul = int(helper.get_command_param_val('--step_mul', remove_from_params=False, default_val=8))
         self._steps_until_rewards_array = np.array([])
+        self._marine_location_per_step = []
+        self._scv_location_per_step = []
+        self._marine_start_location = []
+        self._scv_start_location = []
 
     def step(self, obs):
         # call the parent class to have pysc2 setup rewards/etc
@@ -53,16 +57,27 @@ class MyBaseAgent(base_agent.BaseAgent):
         # if self.steps < 100:
         #     return actions.FunctionCall(_NO_OP, [])
 
-        self.print_step_debug_data()
+        # self.print_step_debug_data()
 
         if obs.reward > 0:
-            r_per_frame = obs.reward / (self._steps_without_rewards * self.step_mul)
+            r_per_frame = obs.reward / (self._steps_without_rewards * self._step_mul)
             msg = f'Reward after {self._steps_without_rewards} steps. R/Frames: {r_per_frame:.2f}'
             my_log.to_file(logging.INFO, msg)
             self._steps_until_rewards_array = np.append(self._steps_until_rewards_array, self._steps_without_rewards)
             self._steps_without_rewards = 0
+
+            my_log.to_file(logging.INFO,f'Marine: {self._marine_location_per_step}')
+            my_log.to_file(logging.INFO,f'SCV: {self._scv_location_per_step}')
+            self._marine_location_per_step = []
+            self._scv_location_per_step = []
+            own_unit_loc, enemy_unit_loc = self._get_units_locations()
+            self._marine_start_location.append(own_unit_loc)
+            self._scv_start_location.append(enemy_unit_loc)
         else:
             self._steps_without_rewards += 1
+            own_unit_loc, enemy_unit_loc = self._get_units_locations()
+            self._marine_location_per_step.append((own_unit_loc, self._get_own_unit_locations()))
+            self._scv_location_per_step.append((enemy_unit_loc, self._get_enemy_unit_locations()))
 
     def reset(self):
         super(MyBaseAgent, self).reset()
@@ -73,25 +88,38 @@ class MyBaseAgent(base_agent.BaseAgent):
             exit()  # There is no setting to quite after x episodes so  this is a hack for it.
 
     def _check_if_deterministic_agent(self):
-        # Here an agent is deterministic if it always takes the same num of steps to get rewards
-        set_version = set(self._steps_until_rewards_array)
-        if len(set_version) == len(self._steps_until_rewards_array):
-            # TODO remove this line once this actually starts to work
-            my_log.to_file(logging.WARNING,
-                           f'DETERMINISTIC! Took {self._steps_until_rewards_array[0]} steps every time')
+        # Unable to confirm number of steps until scv dies but there is cool down env randomness for marine attack
+        # So instead we check if units always start roughly at the same location.
+        location_threshold = 2  # Some bloody random noise in the env that means units do now always spawn at same pxl
+
+        max_marine_x = max([x[0] for x in self._marine_start_location])
+        max_marine_y = max([x[1] for x in self._marine_start_location])
+        min_marine_x = min([x[0] for x in self._marine_start_location])
+        min_marine_y = min([x[1] for x in self._marine_start_location])
+        max_scv_x = max([x[0] for x in self._scv_start_location])
+        max_scv_y = max([x[1] for x in self._scv_start_location])
+        min_scv_x = min([x[0] for x in self._scv_start_location])
+        min_scv_y = min([x[1] for x in self._scv_start_location])
+
+        if (max_marine_x - min_marine_x) > location_threshold:
+            my_log.to_file(logging.INFO, f'NONDETER! Marine_max_x: {max_marine_x}, Marine_min_x: {min_marine_x}')
+        elif (max_marine_y - min_marine_y) > location_threshold:
+            my_log.to_file(logging.INFO, f'NONDETER! Marine_max_y: {max_marine_y}, Marine_min_y: {min_marine_y}')
+        elif (max_scv_x - min_scv_x) > location_threshold:
+            my_log.to_file(logging.INFO, f'NONDETER! scv_max_x: {max_scv_x}, scv_min_x: {min_scv_x}')
+        elif (max_scv_y - min_scv_y) > location_threshold:
+            my_log.to_file(logging.INFO, f'NONDETER! scv_max_y: {max_scv_y}, scv_min_y: {min_scv_y}')
         else:
-            max_steps = max(self._steps_until_rewards_array)
-            min_steps = min(self._steps_until_rewards_array)
-            my_log.to_file(logging.INFO, f'NON-DETERMINISTIC! Steps between {min_steps} and {max_steps} to get rewards')
+            my_log.to_file(logging.INFO, f'DETERMINISTIC AGENT!')
 
-    def _log_units_location(self):
-        own_unit_start_loc_y, own_unit_start_loc_x = self._get_own_unit_locations()
-        own_unit_start_loc = (own_unit_start_loc_x.mean(), own_unit_start_loc_y.mean())
+    def _get_units_locations(self):
+        own_unit_loc_y, own_unit_loc_x = self._get_own_unit_locations()
+        own_unit_loc = (own_unit_loc_x.mean(), own_unit_loc_y.mean())
 
-        enemy_unit_start_loc_y, enemy_unit_start_loc_x = self._get_enemy_unit_locations()
-        enemy_unit_start_loc = (enemy_unit_start_loc_x.mean(), enemy_unit_start_loc_y.mean())
+        enemy_unit_loc_y, enemy_unit_loc_x = self._get_enemy_unit_locations()
+        enemy_unit_loc = (enemy_unit_loc_x.mean(), enemy_unit_loc_y.mean())
 
-        my_log.to_file(logging.DEBUG, f'Marine loc: {own_unit_start_loc}, Enemy loc: {enemy_unit_start_loc}')
+        return own_unit_loc, enemy_unit_loc
 
     def _get_player_relative_view(self):
         """ View from player camera perspective. Returns an NxN np array """
@@ -119,16 +147,16 @@ class AttackAlwaysAgent(MyBaseAgent):
     def step(self, obs):
         super(AttackAlwaysAgent, self).step(obs)
         enemy_y, enemy_x = self._get_enemy_unit_locations()
-        enemy_found = enemy_x.any()
 
         able_to_attack = _ATTACK_SCREEN in self.obs.observation['available_actions']
-        if enemy_found and able_to_attack:
+        if able_to_attack:
             target = [enemy_x.mean(), enemy_y.mean()]
             return actions.FunctionCall(_ATTACK_SCREEN, [_NOT_QUEUED, target])
         else:
-            # Hacky code since enemy sometimes moves out of screen range
+            # For some reason marine is not always able to attack the enemy...
             marine_y, marine_x = self._get_own_unit_locations()
             target = [marine_x.mean() + 1, marine_y.mean()]
+            my_log.to_file(logging.INFO, 'Had to manually select marine')
             return actions.FunctionCall(_SELECT_POINT, [_NOT_QUEUED, target])
 
 
@@ -138,15 +166,15 @@ class AttackMoveAgent(MyBaseAgent):
     def step(self, obs):
         super(AttackMoveAgent, self).step(obs)
         enemy_y, enemy_x = self._get_enemy_unit_locations()
-        enemy_found = enemy_x.any()
 
         able_to_attack = _ATTACK_SCREEN in self.obs.observation['available_actions']
-        if enemy_found and able_to_attack:
+        if able_to_attack:
             target = [enemy_x.mean(), enemy_y.mean()]
             action = _ATTACK_SCREEN if self.steps % 2 == 1 else _MOVE_SCREEN
             return actions.FunctionCall(action, [_NOT_QUEUED, target])
         else:
-            # Hacky code since enemy sometimes moves out of screen range
+            # For some reason marine is not always able to attack the enemy...
             marine_y, marine_x = self._get_own_unit_locations()
             target = [marine_x.mean() + 1, marine_y.mean()]
+            my_log.to_file(logging.INFO, 'Had to manually select marine')
             return actions.FunctionCall(_SELECT_POINT, [_NOT_QUEUED, target])
