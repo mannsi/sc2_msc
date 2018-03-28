3a884a1e5a1a066db4abb7ddda3eebafd14c0253
Mannsi <mannsi1983@gmail.com>
2018-03-28 10:56:39 +0000

    Add propper lstm layer network

--- networks/first_network.py
+++ networks/first_network.py
@@ -3,6 +3,6 @@ def get_network():
         dict(type='conv2d', size=64),
         dict(type='flatten'),
         dict(type='dense', size=32, activation='relu'),
-        # dict(type='lstm', size=64)
+        dict(type='internal_lstm', size=64)
     ]
     return network
