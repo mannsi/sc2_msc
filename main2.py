c11b79f5bdbd011dc22c348d17c2d70c648a911c
mannsi <gudbjorn.einarsson@gmail.com>
2018-04-12 14:30:16 +0000

    Scripts...

--- main2.py
+++ main2.py
@@ -7,9 +7,10 @@ print("Done sleeping, lets add a file to cloud storage")
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--tb_output', dest='tb_output', type=str)
+parser.add_argument('--agent', dest='agent', type=str)
 args = parser.parse_args()
 
-print("got arg ", args.tb_output)
+print("got arg ", args.tb_output, args.agent)
 
 with open(args.tb_output + '/from_python.txt', 'w') as f:
     f.write("text from python")
