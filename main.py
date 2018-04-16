192c350fe7391e64f8416f0154bb7ee08f804f4a
mannsi <gudbjorn.einarsson@gmail.com>
2018-04-16 12:59:20 +0000

    Small refactoring of params

--- main.py
+++ main.py
@@ -27,21 +27,19 @@ flags.DEFINE_float("discount", 0.99, "Discount rate for future rewards.")
 flags.DEFINE_enum("agent_race", None, sc2_env.races.keys(), "Agent's race.")
 flags.DEFINE_enum("bot_race", None, sc2_env.races.keys(), "Bot's race.")
 flags.DEFINE_enum("difficulty", None, sc2_env.difficulties.keys(), "Bot's strength.")
-flags.DEFINE_integer("snapshot_step", int(1e1), "Step for snapshot.")  # I use this to run the agent without training
+flags.DEFINE_integer("snapshot_step", 1, "Step for snapshot.")  # I use this to run the agent without training
 flags.DEFINE_string("snapshot_path", "./snapshot/", "Path for snapshot.")
-flags.DEFINE_string("log_path", "/home/mannsi/Repos/sc2_msc/log/", "Path for log.")
+flags.DEFINE_string("log_path", "/home/mannsi/code/sc2_msc/log/", "Path for log.")
 flags.DEFINE_string("device", "0", "Device for training.")
 flags.DEFINE_bool("profile", False, "Whether to turn on code profiling.")
 flags.DEFINE_bool("trace", False, "Whether to trace the code execution.")
 flags.DEFINE_integer("minimap_resolution", 84, "Resolution for minimap feature layers.")
 
 flags.DEFINE_string("map", "DefeatLing", "Name of a map to use.")
-flags.DEFINE_integer("max_steps", 10, "Total steps for training.")  # Num episodes
+flags.DEFINE_integer("max_steps", 20, "Total steps for training.")  # Num episodes
 flags.DEFINE_bool("render", False, "Whether to render with pygame.")
 flags.DEFINE_integer("screen_resolution", 84, "Resolution for screen feature layers.")
 flags.DEFINE_integer("step_mul", 8, "Game steps per agent step.")
-flags.DEFINE_integer("max_agent_steps", 100,
-                     "Total agent steps.")  # Max agent steps per episode. Runs these steps or until it finishes. Not used
 flags.DEFINE_string("agent", "always_attack", "Which agent to run.")
 flags.DEFINE_string("net", "atari", "atari or fcn.")
 
@@ -49,7 +47,6 @@ flags.DEFINE_integer("episodes_between_updates", 1, "How many episodes to run be
 flags.DEFINE_bool("randomize_replay_buffer", True, "Randomize the replay buffer before updating an agent")
 flags.DEFINE_bool("test_agent", True, "To run agent both in training and test mode")
 flags.DEFINE_string("run_comment", "Normal", "A comment string to distinguish the run.")
-flags.DEFINE_string("replay", "/home/mannsi/Repos/sc2_msc/log/table/DefeatLing/8/fixed_lr/2/Replays/DefeatLing_2018-04-15-12-44-04.SC2Replay", "Path of file to replay from. Requires training flag as false")
 
 FLAGS(sys.argv)
 BASE_LOG_PATH = os.path.join(FLAGS.log_path, FLAGS.agent, FLAGS.map, str(FLAGS.step_mul))
